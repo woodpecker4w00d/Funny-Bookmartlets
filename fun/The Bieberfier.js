6cc3cfb75bc0014fc433718b67e40a38a5d14c9f
Woodpecker3w00d_ <113268712+woodpecker4w00d@users.noreply.github.com>
2024-09-08 16:46:27 -0700

    Add files via upload

---
+++ fun/The Bieberfier.js
@@ -0,0 +1,3 @@
+//Changes all images on the page to Justin Beiber
+
+javascript:(function(){Array.prototype.slice.call(document.querySelectorAll('img')).map(function(el){el.src = '//codebox.org.uk/graphics/bieber.jpg';});}())
